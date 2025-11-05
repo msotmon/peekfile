3457ff7225cc9f3bf56879d5c90100ec6a020a46
msotmon <msotelomontoro@gmail.com>
2025-11-05 21:12:40 +0100

    Default 3

--- peek.sh
+++ peek.sh
@@ -2,9 +2,22 @@ file="$1"
 
 num="$2"
 
+if [[ -z "$2" ]]; then
+
+echo "Imprimiendo 3 primeras y 3 ultimas lineas por defecto"
+
+head -n 3  "$file"
+
+echo "..."
+
+tail -n 3  "$file"
+
+else 
 
 head -n "$num" "$file"
 
 echo "..."
 
 tail -n "$num" "$file"
+
+fi
